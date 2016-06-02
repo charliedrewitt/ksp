b8cf65e7bfc09fb3b16e3243a683471724a028bc
charliedrewitt <charlie.drewitt@gmail.com>
2016-06-02 19:51:56 +0100

    Preparing for release. - Removing unnecessary files. - Adding copyright notices and copy of gpl.

--- AssemblyFuelUtility/FuelModel.cs
+++ AssemblyFuelUtility/FuelModel.cs
@@ -1,4 +1,24 @@
-﻿using CDKspUtil.Logic;
+﻿#region Copyright Notice
+
+// Copyright 2016 Charlie Drewitt
+
+// This file is part of Assembly Fuel Utility.
+
+// Assembly Fuel Utility is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Assembly Fuel Utility is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Assembly Fuel Utility.  If not, see<http://www.gnu.org/licenses/>.
+
+#endregion
+using CDKspUtil.Logic;
 using KSP.UI;
 using System;
 using System.Collections.Generic;
